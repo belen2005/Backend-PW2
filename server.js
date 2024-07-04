b804c2e75997a45913816119fb68ff54ae9bb7ab
Belen <contactobelends@gmail.com>
2024-07-04 19:20:44 -0300

    server file changed

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-import express from 'express';
-const app = express()
-const port = process.env.PORT || 3000;
+import express from "express";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('API HOBBIES')
-})
+const app = express();
+const port = process.env.PORT || 3000;
+const corsOptions = {
+  origin: "https://hobby-pw2.netlify.app",
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("API HOBBIES");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
